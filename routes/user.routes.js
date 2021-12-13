3da0711d50306ce977f09192d0e16461c7b89ccd
spykelion <ndilionel6@gmail.com>
2021-12-13 07:29:25 +0100

    minute changes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router({ strict: true });
-
+const auth = require('../auth/verify')
 const user = require('../controllers/user.controller')
 
 router
-  .get("/", user.getAll)
-  .get("/:id", user.getOne)
+  .get("/", auth,user.getAll)
+  .get("/:id",auth, user.getOne)
   .post("/", user.create)
   .post('/login', user.login)
-  .post('/auth/:code', user.authenticate)
-  .patch("/:id", user.update)
-  .delete('/all', user.deleteAll)
-  .delete("/:id", user.deleteOne);
+  .post('/auth/:code',auth, user.authenticate)
+  .patch("/:id", auth,user.update)
+  // .delete('/all',auth, user.deleteAll)
+  // .delete("/:id", auth,user.deleteOne);
 
 module.exports = router;
